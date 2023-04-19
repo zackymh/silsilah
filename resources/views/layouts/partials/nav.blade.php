4a0272e9a186605e6243cf4c455581810384ea47
zackymh <76012435+zackymh@users.noreply.github.com>
2023-04-20 01:16:21 +0700

    perubahan Brand

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -12,7 +12,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+                Bani Madnur
             </a>
         </div>
 
