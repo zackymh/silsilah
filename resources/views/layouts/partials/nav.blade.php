0fb79a8a931970475555ec2c740c22db7fbf0b82
zackymh <zidraka@gmail.com>
2023-04-21 18:19:18 +0700

    sembunyikan register nav

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -31,7 +31,6 @@
                 <li><a href="{{ url(url()->current() . $mark . 'lang=id') }}">id</a></li>
                 @if (Auth::guest())
                     <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
